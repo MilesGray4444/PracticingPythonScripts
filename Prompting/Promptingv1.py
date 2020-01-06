68e59778e171a624a213e50a77735ef1198056df
MilesGray444 <SeongHong@Macbook-Pro.local>
2020-01-06 15:09:22 +0900

    put prompting related scripts to a seperate file

---
+++ Prompting/Promptingv1.py
@@ -0,0 +1,11 @@
+# Thank you Zed Shaw for teaching me how to learn python the hard way.
+print("How old could you possibly be?", end=' ')
+age = int(input())
+print("How tall are you?", end=' ')
+height = str(input())
+print("How much do you weigh in kilograms my friend?", end=' ')
+weight = input()
+
+print(f"\nSo my friend, you are {age} years old, {height} tall and {weight}kgs heavy.")
+
+print("\n\t Thank you for answering my questions!")
