0de1dad95ecafb4db20250519438facde1a9c594
MilesGray444 <SeongHong@Macbook-Pro.local>
2020-01-06 14:36:22 +0900

    added bunch of scripts written in .py

---
+++ Promptingv2.py
@@ -0,0 +1,8 @@
+# easier prompting using python
+
+age = int(input("How old are you? "))
+height = str(input("How tall are you? "))
+weight = str(input("How much do you weigh? "))
+
+print(f"So let's see, you're {age} old, {height} tall and {weight} heavy.")
+print("\n Once again, Thank you for answering these questions!")
---
+++ SetParaUnpackVars.py
@@ -0,0 +1,9 @@
+from sys import argv
+# run it on termianl with python3, works on 2 as well though
+# make sure you run my script python  <name>.py  <variable1>  <variable2>  <variable3>
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your final variable is:", third)
